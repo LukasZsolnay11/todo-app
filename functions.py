a604eef1726a1441c6d7bb0704d4702048dcd767
lukas <L>
2023-01-21 11:27:15 +0100

    add button

--- functions.py
+++ functions.py
@@ -5,3 +5,6 @@ def get_todos():
         todos = file.readlines()
     return todos
 
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -1,10 +1,26 @@
+import functions
 from functions import get_todos
 import PySimpleGUI as sq
 
 label = sq.Text("Type in a to-do")
-input_box = sq.InputText(tooltip="Enter todo")
+input_box = sq.InputText(tooltip="Enter todo", key="todo")
 add_button = sq.Button("Add")
 
-window = sq.Window('To-Do App', layout=[[label, input_box, add_button]])
-window.read()
+window = sq.Window('To-Do App',
+                   layout=[[label, input_box, add_button]],
+                   font=('Helvetica', 10))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sq.WIN_CLOSED:
+            break
+
 window.close()
