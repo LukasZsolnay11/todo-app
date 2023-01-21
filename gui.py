854e59fdf53501986fa23d51c8a584d747504c46
lukas <L>
2023-01-21 08:11:23 +0100

    create basic GUI

---
+++ gui.py
@@ -0,0 +1,10 @@
+from functions import get_todos
+import PySimpleGUI as sq
+
+label = sq.Text("Type in a to-do")
+input_box = sq.InputText(tooltip="Enter todo")
+add_button = sq.Button("Add")
+
+window = sq.Window('To-Do App', layout=[[label,], [input_box, add_button]])
+window.read()
+window.close()
