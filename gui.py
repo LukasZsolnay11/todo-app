89367d0d921d0bb77d9a78a17aadd11a34c0fd56
lukas <L>
2023-01-22 09:04:36 +0100

    complette button

--- gui.py
+++ gui.py
@@ -9,10 +9,14 @@ list_box = sq.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 
 edit_button = sq.Button("Edit")
+complete_button = sq.Button("Complete")
+exit_button = sq.Button("Exit")
 
 window = sq.Window('To-Do App',
-                   layout=[[label, input_box, add_button],[list_box, edit_button]],
-                   font=('Helvetica', 10))
+                   layout=[[label, input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                    font=('Helvetica', 10))
 
 while True:
     event, values = window.read()
@@ -36,6 +40,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sq.WIN_CLOSED:
